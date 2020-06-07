Fixed covariance

kalman.py
@@ -14,27 +14,25 @@ v = lambda x: 20*np.heaviside(x-30, 0) - 10*np.heaviside(x-70, 0) + 10*np.sin(0.
 if __name__ == "__main__":
 
     fig, axs = plt.subplots(3)
+    dt = 0.1
 
-    tv = np.arange(0, 150, 0.25)
+    tv = np.arange(0, 150, dt)
     RS = state.RobotState(function=v, time_vector=tv)
 
-    # sensors = [Encoder(RS), GPS(RS), IMU(RS)]
-    sensors = [DummySensor(RS.vel_vect, 2, 7), DummySensor(RS.vel_vect, 10, 4), DummySensor(RS.vel_vect, 50, 0.04)]
-    kf = KalmanFilter(sensors, tv, 0.01)
+    sensors = [DummySensor(RS.vel_vect, 2, 0.5), IMU(RS), GPS(RS)]
+    kf = KalmanFilter(sensors, tv, 0.1, dt)
     estimation = kf.estimate()
 
     axs[0].plot(RS.time_vector, RS.vel_vect, "--", linewidth=1, color="grey")
     axs[0].plot(RS.time_vector, estimation, linewidth=0.7)
     for sensor in sensors:
-        axs[0].plot(RS.time_vector[0::sensor.freq], sensor.observation_in_time()[0::sensor.freq], "--", linewidth=0.2)
+        axs[0].plot(RS.time_vector[0::sensor.freq], sensor.observation_in_time()[0::sensor.freq], "--", linewidth=0.3)
     axs[0].legend(["True state", "Estimated state", "Encoder", "IMU", "GPS"])
     axs[0].axes.set_xlabel("time [s]")
     axs[0].axes.set_ylabel("velocity [m/s]")
-    
     axs[1].plot(RS.time_vector, kf.P_in_time(), linewidth=0.5)
     axs[1].axes.set_xlabel("time [s]")
     axs[1].axes.set_ylabel("covariance")
-
     axs[2].plot(RS.time_vector, RS.vel_vect - estimation, linewidth=0.5)
     axs[2].plot(RS.time_vector, np.zeros(len(RS.time_vector)), "--", linewidth=0.5, color="grey")
     axs[2].axes.set_xlabel("time [s]")

kalman/kalman_filter/kalman_filter.py
@@ -2,14 +2,15 @@ import numpy as np
 
 
 class KalmanFilter:
-    def __init__(self, sensors, time_vector, Q):
+    def __init__(self, sensors, time_vector, process_noise, dt):
         self.ts_prev = 0.0
         self._sensors = sensors
         self._time_vector = time_vector
+        self._can_predict = True
 
         self._x = np.array([0.0])
         self._P = np.array([0.5])
-        self._Q = np.array([Q])
+        self._Q = np.array([dt]) * process_noise
         self._F = np.array([1])
 
         self._P_in_time = np.zeros(len(self._time_vector))
@@ -38,7 +39,7 @@ class KalmanFilter:
 
 
     def _update(self, sensor):
-        S = sensor.H * self._P * sensor.H + sensor.R
+        S = sensor.H * self._P * sensor.H.T + sensor.R
         K = self._P * sensor.H.T * S**(-1)
         self._x = self._x + (K * (sensor.z - sensor.H * self._x))
         self._P = (1 - K * sensor.H) * self._P

kalman/sensor/dummySensor.py
@@ -22,7 +22,7 @@ class DummySensor(SensorInterface):
 
     @property
     def R(self):
-        return np.array([self.noise_std])
+        return np.array([self.noise_std**2])
 
     @property
     def H(self):

kalman/sensor/gps.py
@@ -11,8 +11,8 @@ class GPS(SensorInterface):
         self._raw_velocity = RS.vel_vect
         self._position = integrate.cumtrapz(self._raw_velocity, initial=0)
         self._index = 0  # indeks obecnie przetwarzanej probki danych
-        self._noise_std = 4
-        self._frequency = 10  # czestotliwosc pracy czujnika
+        self._noise_std = 0.01
+        self._frequency = 30  # czestotliwosc pracy czujnika
         self.is_data_available = False
         self._add_noise()  # dodaj szum do position
         self._velocity = np.diff(self._position)
@@ -26,7 +26,7 @@ class GPS(SensorInterface):
 
     @property
     def R(self):
-        return np.array([self._noise_std])
+        return np.array([self._noise_std**2])
 
     @property
     def H(self):

kalman/sensor/imu.py
@@ -10,8 +10,8 @@ class IMU(SensorInterface):
         super().__init__(RS)
         self.raw_velocity = RS.vel_vect
         self.index = 0  # indeks obecnie przetwarzanej probki danych
-        self.noise_std = 0.1 # TODO jakie odchylenie standardowe szumu czujnika?
-        self.frequency = 5  # czestotliwosc pracy czujnika
+        self.noise_std = 0.5 # TODO jakie odchylenie standardowe szumu czujnika?
+        self.frequency = 7  # czestotliwosc pracy czujnika
         self.acceleration = np.diff(self.raw_velocity)
         self.is_data_available = False
         self._add_noise()  # dodaj szum do position
@@ -27,7 +27,7 @@ class IMU(SensorInterface):
 
     @property
     def R(self):
-        return np.array([self.noise_std])
+        return np.array([self.noise_std**2])
 
     @property
     def H(self):